e716820fc0147012d1c9e46858a7244af18de9cd
wenbo <wenmobo2018@gmai.com>
2021-07-09 22:59:43 +0800

    add demo

--- 源码架构/ReactiveObjC/Code/Demo/Demo/ViewController.m
+++ 源码架构/ReactiveObjC/Code/Demo/Demo/ViewController.m
@@ -27,35 +27,42 @@
     RACCommand *command = [[RACCommand alloc] initWithSignalBlock:^RACSignal * _Nonnull(id  _Nullable input) {
         return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
 //            [subscriber sendNext:@"2"];
-            NSError *error = [NSError errorWithDomain:NSCocoaErrorDomain code:1 userInfo:@{}];
-            [subscriber sendError:error];
+            
+//            NSError *error = [NSError errorWithDomain:NSCocoaErrorDomain code:1 userInfo:@{}];
+//            [subscriber sendError:error];
 //            [subscriber sendCompleted];
+            NSLog(@"执行了command");
             return nil;
         }];
     }];
+    self.command.allowsConcurrentExecution = YES;
     self.command = command;
     
-    [[command.executionSignals flattenMap:^__kindof RACSignal * _Nullable(id  _Nullable value) {
-        return value;
-    }] subscribeNext:^(id  _Nullable x) {
-        NSLog(@"%@", x);
-    }];
-    
-    [self.command.executionSignals.switchToLatest subscribeNext:^(id  _Nullable x) {
-        NSLog(@"switchToLatest = %@", x);
-    }];
-    
-    [self.command.errors subscribeNext:^(NSError * _Nullable x) {
-        NSLog(@"error = %@", x);
-    }];
-    
-    [[self.command execute:@"1"] subscribeNext:^(id  _Nullable x) {
-        NSLog(@"signal = %@", x);
-    } error:^(NSError * _Nullable error) {
-        NSLog(@"error");
-    } completed:^{
-        NSLog(@"completed");
-    }];
+//    [[command.executionSignals flattenMap:^__kindof RACSignal * _Nullable(id  _Nullable value) {
+//        return value;
+//    }] subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"%@", x);
+//    }];
+//
+//    [self.command.executionSignals.switchToLatest subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"switchToLatest = %@", x);
+//    }];
+//
+//    [self.command.errors subscribeNext:^(NSError * _Nullable x) {
+//        NSLog(@"error = %@", x);
+//    }];
+//
+//    [[self.command execute:@"1"] subscribeNext:^(id  _Nullable x) {
+//        NSLog(@"signal = %@", x);
+//    } error:^(NSError * _Nullable error) {
+//        NSLog(@"error");
+//    } completed:^{
+//        NSLog(@"completed");
+//    }];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.command execute:nil];
 }
 
 
